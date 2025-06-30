29ac6e6b6f08757c01e19b18779816c3d31c6f42
Airat <airat.sharushev@gmail.com>
2025-06-30 17:12:28 +0300

    kafka работает

--- init.sql
+++ init.sql
@@ -15,3 +15,43 @@ CREATE TABLE IF NOT EXISTS orders (
     date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     oof_shard TEXT
 );
+CREATE TABLE deliveries (
+    order_uid TEXT PRIMARY KEY,
+    name TEXT,
+    phone TEXT,
+    zip TEXT,
+    city TEXT,
+    address TEXT,
+    region TEXT,
+    email TEXT
+);
+
+CREATE TABLE payments (
+    order_uid TEXT PRIMARY KEY,
+    transaction TEXT,
+    request_id TEXT,
+    currency TEXT,
+    provider TEXT,
+    amount INT,
+    payment_dt BIGINT,
+    bank TEXT,
+    delivery_cost INT,
+    goods_total INT,
+    custom_fee INT
+);
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    order_uid TEXT,
+    chrt_id INT,
+    track_number TEXT,
+    price INT,
+    rid TEXT,
+    name TEXT,
+    sale INT,
+    size TEXT,
+    total_price INT,
+    nm_id INT,
+    brand TEXT,
+    status INT
+);
